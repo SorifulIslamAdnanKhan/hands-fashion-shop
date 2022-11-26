8b874b0cd2e8bf1684b6f753b9826e4e6a3a3465
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-11-26 20:46:12 +0600

    404 page designed

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import error from '../assets/images/error.png';
 
 const Error = () => {
     return (
-        <div>
-            
+        <div className="hero mt-32 p-2">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={error} className="max-w-sm rounded-lg shadow-2xl" alt='' />
+                <div className='text-center'>
+                    <h1 className="text-5xl font-bold">Opps! Page Not Found.</h1>
+                    <p className="py-6">Plesae go back to home page.</p>
+                    <Link to='/'><button className="btn btn-primary">Home</button></Link>
+                </div>
+            </div>
         </div>
     );
 };
