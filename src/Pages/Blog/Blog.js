1a724cd9110352ec0aea882334d9feb1ee9ec80e
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-11-28 09:39:15 +0600

    categories displayed to home page

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -9,7 +9,26 @@ const Blog = () => {
                     1.  What are the different ways to manage a state in a React application?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <article className='text-lg p-6'>
+                        <p className='p-4'>
+                            There are four main types of state you need to properly manage in your React apps:
+                            <ol className='list-decimal p-4'>
+                                <li>Local State</li>
+                                <li>Server State</li>
+                                <li>Global State</li>
+                                <li>URL State</li>
+                            </ol>
+                            Let's talk about how you can manage these states different ways. <br />
+
+                            1. Local State: Local state is most often managed in React using the <strong>useState</strong> hook. <br />
+
+                            2. Server State <br />
+
+                            3. Global State: Global state is data we manage across many components. It is necessary when you want to get and update data anywhere in your app, or in multiple components at least.
+
+                        </p>
+
+                    </article>
                 </div>
             </div>
             <br />
@@ -18,7 +37,7 @@ const Blog = () => {
                     2. How does prototypical inheritance work?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={1} attribute is necessary to make the div focusable</p>
+                    <p> attribute is necessary to make the div focusable</p>
                 </div>
             </div>
             <br />
@@ -27,7 +46,7 @@ const Blog = () => {
                     3. What is a unit test? Why should we write unit tests?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={2} attribute is necessary to make the div focusable</p>
+                    <p>attribute is necessary to make the div focusable</p>
                 </div>
             </div>
             <br />
@@ -36,7 +55,7 @@ const Blog = () => {
                     4. React vs. Angular vs. Vue?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={3} attribute is necessary to make the div focusable</p>
+                    <p> attribute is necessary to make the div focusable</p>
                 </div>
             </div>
         </section>
--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,58 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { BsSmartwatch } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    const url = 'http://localhost:5000/categories';
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setCategories(response.data);
+        });
+    }, []);
+
+
     return (
         <section className='mt-8 p-6'>
             <h2 className="text-3xl text-center p-4">Product Categories</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-                <div className="card bg-base-100 shadow-xl">
+                {
+                    categories.map(category => <div key={category._id} className="card bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                        <BsSmartwatch className='text-amber-600 text-4xl' />
                     </figure>
                     <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <h2 className="card-title">{category.name}</h2>
                         <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
+                            <Link><button className="btn btn-primary">View Products</button></Link>
                         </div>
                     </div>
-                </div>
-                <div className="card bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
+                </div>)
+                }
             </div>
         </section>
     );
